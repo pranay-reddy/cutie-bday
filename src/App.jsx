2ce6a88dc07050f918960cd0033f76f335f72e5e
pranay reddy <bojannagari03.ugcs20@iiitranchi.ac.in>
2025-12-08 06:55:08 +0530

    changed name from app to temp

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useAuth } from "./context/AuthContext";
-import "./App.css";
+import "./Temp.css";
 
 import Navbar from "./components/Navibar";
 import ProfileBar from "./components/ProfileBar";
