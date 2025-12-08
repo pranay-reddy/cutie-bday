e1eed1b03e87dc2033056753dec034f8f36ab539
pranay reddy <bojannagari03.ugcs20@iiitranchi.ac.in>
2025-12-08 06:40:37 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useAuth } from "./context/AuthContext";
-import "./APP.css";
+import "./App.css";
 
 import Navbar from "./components/Navibar";
 import ProfileBar from "./components/ProfileBar";
